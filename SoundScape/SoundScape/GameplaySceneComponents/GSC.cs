24fbde83ef93de6109a68511312d970a6d357ef6
John Steel <jsteel-cc@conestogac.on.ca>
2014-11-19 09:48:39 -0500

    Added two untracked files.

---
+++ SoundScape/SoundScape/GameplaySceneComponents/GSC.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace SoundScape.GameplaySceneComponents
+{
+    class GSC : DrawableGameComponent
+    {
+        private SpriteBatch spriteBatch;
+        private SoundEffect soundEffect;
+        private Texture2D texture;
+        private Color colour;
+        private Vector2 position;
+        private Rectangle hitbox;
+
+        protected GSC(Game game, SpriteBatch spriteBatch, Vector2 position, Texture2D texture, SoundEffect sound, Rectangle hitbox) : base(game)
+        {
+            SpriteBatch = spriteBatch;
+            Position = position;
+            Texture = texture;
+            SoundEffect = sound;
+            Hitbox = hitbox;
+            Colour = Color.White;
+        }
+
+        protected GSC(Game game, SpriteBatch spriteBatch, Vector2 position, Texture2D texture, SoundEffect sound, Rectangle hitbox,
+            Color colour) : this(game, spriteBatch, position, texture, sound, hitbox)
+        {
+            Colour = colour;
+        }
+
+        protected SpriteBatch SpriteBatch
+        {
+            get { return spriteBatch; }
+            set { spriteBatch = value; }
+        }
+
+        protected SoundEffect SoundEffect
+        {
+            get { return soundEffect; }
+            set { soundEffect = value; }
+        }
+
+        protected Texture2D Texture
+        {
+            get { return texture; }
+            set { texture = value; }
+        }
+
+        protected Color Colour
+        {
+            get { return colour; }
+            set { colour = value; }
+        }
+
+        protected Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        protected Rectangle Hitbox
+        {
+            get { return hitbox; }
+            set { hitbox = value; }
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            SpriteBatch.Begin();
+            SpriteBatch.Draw(texture, Position, Colour);
+            SpriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ SoundScape/SoundScape/GameplaySceneComponents/Player.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+namespace SoundScape.GameplaySceneComponents
+{
+    class Player : GSC
+    {
+        private PlayerIndex controllerIndex;
+        private GamePadState padState;
+        private GamePadState padOldState;
+
+
+        public Player(Game game, SpriteBatch spriteBatch, Vector2 position, Texture2D texture, 
+            SoundEffect soundEffect, Rectangle hitbox)
+            : base(game, spriteBatch, position, texture, soundEffect, hitbox)
+        {
+        }
+
+        public Player(Game game, SpriteBatch spriteBatch, Vector2 position, Texture2D texture, 
+            SoundEffect soundEffect, Rectangle hitbox, Color colour)
+            : base(game, spriteBatch, position, texture, soundEffect, hitbox, colour)
+        {
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            padState = GamePad.GetState(ControllerIndex);
+            float deltaX = padState.ThumbSticks.Left.X;
+            float deltaY = -padState.ThumbSticks.Left.Y;
+
+            if (padState.IsConnected &&
+                padState.DPad.Down == ButtonState.Pressed &&
+                padState.DPad.Down == ButtonState.Released)
+                Console.WriteLine(padState.DPad.Down);
+
+            Position += new Vector2(deltaX*3, deltaY*3);
+            padOldState = padState;
+            base.Update(gameTime);
+        }
+
+        public override void Draw(GameTime gameTime)
+        {
+            var arrow = new Vector2(padState.ThumbSticks.Right.X, -padState.ThumbSticks.Right.Y) * 100;
+            //if (!arrow.Equals(Vector2.Zero))
+            {
+
+                SpriteBatch.Begin();
+                SpriteBatch.Draw(Texture, Position + arrow, Colour);
+                SpriteBatch.End();
+            }
+            base.Draw(gameTime);
+        }
+
+
+        public PlayerIndex ControllerIndex
+        {
+            get { return controllerIndex; }
+            set { controllerIndex = value; }
+        }
+    }
+}
