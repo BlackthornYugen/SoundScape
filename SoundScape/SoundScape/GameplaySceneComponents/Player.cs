8ae72ee37ed7c231b38c80af1501f89342fc8a95
John Steel <jsteel-cc@conestogac.on.ca>
2014-12-04 18:46:59 -0500

    Reorginized Player.cs

--- SoundScape/SoundScape/GameplaySceneComponents/Player.cs
+++ SoundScape/SoundScape/GameplaySceneComponents/Player.cs
@@ -7,49 +7,54 @@ using Microsoft.Xna.Framework.Input;
 namespace SoundScape.GameplaySceneComponents
 {
     /// <summary>
-    /// A player object
+    ///     A player object
     /// </summary>
-    class Player : GameplaySceneComponent
+    internal class Player : GameplaySceneComponent
     {
         private const int DISTANCE_FACTOR = 30;
         private const float PITCH_VELOCITY = 0.01f;
         private const float TOLLERENCE = 0.01f;
         private const int SCORE = -50;
-
-        private VirtualController _controller;
-        private Vector2 _arrow;
-        private Vector2[] _aimVectors;
-        private float _pan;
-        private SoundEffectInstance _activeSound;
-        private float _rumbleLeft, _rumbleRight;
-        private DateTime _rumbleLeftTime, _rumbleRightTime;
         private readonly SoundEffectInstance _weaponSoundEffectInstance;
+        private SoundEffectInstance _activeSound;
+
+        private Vector2[] _aimVectors;
+        private Vector2 _arrow;
+        private VirtualController _controller;
+        private float _pan;
+        private float _rumbleLeft;
+        private DateTime _rumbleLeftTime;
+        private float _rumbleRight;
+        private DateTime _rumbleRightTime;
         private WeaponState _weaponState = WeaponState.Discharged;
 
-        private enum WeaponState
-        {
-            Charged,
-            Charging,
-            Discharged,
-            Cooldown
-        }
-
         /// <summary>
-        /// Construct a player with a specific colour
+        ///     Construct a player with a specific colour
         /// </summary>
-        /// <param name="scene">A reference to the scene that the player 
-        /// will inhabit.</param>
-        /// <param name="spriteBatch">A reference to a spritebatch 
-        /// object.</param>
-        /// <param name="position">The starting position of the player.
+        /// <param name="scene">
+        ///     A reference to the scene that the player
+        ///     will inhabit.
+        /// </param>
+        /// <param name="spriteBatch">
+        ///     A reference to a spritebatch
+        ///     object.
+        /// </param>
+        /// <param name="position">
+        ///     The starting position of the player.
         /// </param>
         /// <param name="texture">The player's texture.</param>
-        /// <param name="soundEffect">The soundeffect that identifies this 
-        /// player.</param>
-        /// <param name="pan">The audio channel that sound is played on for 
-        /// this player where -1 is full left and 1 is full right.</param>
-        /// <param name="weaponSoundEffect">The sound used for the player's
-        /// weapon.</param>
+        /// <param name="soundEffect">
+        ///     The soundeffect that identifies this
+        ///     player.
+        /// </param>
+        /// <param name="pan">
+        ///     The audio channel that sound is played on for
+        ///     this player where -1 is full left and 1 is full right.
+        /// </param>
+        /// <param name="weaponSoundEffect">
+        ///     The sound used for the player's
+        ///     weapon.
+        /// </param>
         /// <param name="colour">The colour to draw the player.</param>
         public Player(GameplayScene scene, SpriteBatch spriteBatch, Vector2 position, Texture2D texture,
             SoundEffect soundEffect, float pan, SoundEffect weaponSoundEffect, Color colour)
@@ -62,7 +67,62 @@ namespace SoundScape.GameplaySceneComponents
         }
 
         /// <summary>
-        /// Update movement, handle collision and handle rumbling
+        ///     The left motor's rumble percentage.
+        /// </summary>
+        public float RumbleLeft
+        {
+            get { return _rumbleLeft; }
+            set
+            {
+                if (Math.Abs(_rumbleLeft - value) > TOLLERENCE)
+                {
+                    _rumbleLeft = value;
+                    GamePad.SetVibration(Controller.PlayerIndex, _rumbleLeft, _rumbleRight);
+                }
+            }
+        }
+
+        /// <summary>
+        ///     The right motor's rumble percentage.
+        /// </summary>
+        public float RumbleRight
+        {
+            get { return _rumbleRight; }
+            set
+            {
+                if (Math.Abs(_rumbleRight - value) > TOLLERENCE)
+                {
+                    _rumbleRight = value;
+                    GamePad.SetVibration(Controller.PlayerIndex, _rumbleLeft, _rumbleRight);
+                }
+            }
+        }
+
+
+        /// <summary>
+        ///     Control the audio channel that sound is played on for this player.
+        /// </summary>
+        public float Pan
+        {
+            get { return _pan; }
+            set { _pan = value; }
+        }
+
+        public override int Score
+        {
+            get { return SCORE; }
+        }
+
+        public Texture2D SonarTexture { get; set; }
+
+        public VirtualController Controller
+        {
+            get { return _controller; }
+            set { _controller = value; }
+        }
+
+        /// <summary>
+        ///     Update movement, handle collision and handle rumbling
         /// </summary>
         /// <param name="gameTime"></param>
         public override void Update(GameTime gameTime)
@@ -89,9 +149,9 @@ namespace SoundScape.GameplaySceneComponents
             // Movement
             float deltaX = Controller.MovementAxisX;
             float deltaY = -Controller.MovementAxisY;
-            var oldPosition = Position;
+            Vector2 oldPosition = Position;
             Position += new Vector2(deltaX*3, deltaY*3);
-            
+
             foreach (IGameComponent component in Scene.Components)
             {
                 var gsc = component as GameplaySceneComponent;
@@ -108,10 +168,11 @@ namespace SoundScape.GameplaySceneComponents
                         {
                             Position = oldPosition;
                             // Rumble for the player coliding.
-                            RumbleFor(miliseconds: 75, leftMotor: 1);
+                            RumbleFor(75, 1);
                             if (gsc is Player)
-                            {   // Rumble for other player
-                                (gsc as Player).RumbleFor(miliseconds: 50, rightMotor: 1);
+                            {
+                                // Rumble for other player
+                                (gsc as Player).RumbleFor(50, rightMotor: 1);
                             }
                         }
                     }
@@ -144,26 +205,26 @@ namespace SoundScape.GameplaySceneComponents
                 _weaponSoundEffectInstance.Pitch = -1;
                 _weaponSoundEffectInstance.Play();
                 _weaponState = WeaponState.Charging;
-                Colour = new Color(Colour.R / 2, Colour.G / 2, Colour.B / 2);
+                Colour = new Color(Colour.R/2, Colour.G/2, Colour.B/2);
             }
 
             // Sound Wave
-            _arrow = new Vector2(_controller.AimAxisX, -_controller.AimAxisY) * DISTANCE_FACTOR;
+            _arrow = new Vector2(_controller.AimAxisX, -_controller.AimAxisY)*DISTANCE_FACTOR;
 
             bool hitSomething = false;
 
-            var arrowLength = (int)_arrow.Length();
+            var arrowLength = (int) _arrow.Length();
 
             if (_weaponState == WeaponState.Charged ||
                 _weaponState == WeaponState.Charging)
                 // Shots fired go DISTANCE_FACTOR further than sonar.
-                arrowLength *= DISTANCE_FACTOR; 
+                arrowLength *= DISTANCE_FACTOR;
 
             _aimVectors = new Vector2[arrowLength];
 
             for (int i = 0; i < arrowLength && !hitSomething; i++)
             {
-                _aimVectors[i] = Position + _arrow * i;
+                _aimVectors[i] = Position + _arrow*i;
                 foreach (IGameComponent gameComponent in Scene.Components)
                 {
                     var gsc = gameComponent as GameplaySceneComponent;
@@ -182,22 +243,22 @@ namespace SoundScape.GameplaySceneComponents
             bool hitSomething = false;
             if (gsc.Hitbox.Contains((int) _aimVectors[i].X, (int) _aimVectors[i].Y))
             {
-                if (_weaponState == WeaponState.Charged || 
+                if (_weaponState == WeaponState.Charged ||
                     _weaponState == WeaponState.Charging)
                 {
-                        _weaponSoundEffectInstance.Pitch = 1;
-                        _weaponSoundEffectInstance.Play();
-                        _weaponState = WeaponState.Cooldown;
-                        Colour = new Color(Colour.R * 2, Colour.G * 2, Colour.B * 2);
-                        gsc.Kill();
+                    _weaponSoundEffectInstance.Pitch = 1;
+                    _weaponSoundEffectInstance.Play();
+                    _weaponState = WeaponState.Cooldown;
+                    Colour = new Color(Colour.R*2, Colour.G*2, Colour.B*2);
+                    gsc.Kill();
                 }
                 else if (_weaponState != WeaponState.Charging &&
-                    _weaponState != WeaponState.Cooldown)
+                         _weaponState != WeaponState.Cooldown)
                 {
                     hitSomething = true;
                     if (_activeSound == null || _activeSound.State == SoundState.Stopped)
                     {
-                        SonarHit(gsc, 1f - (float)i / limit);
+                        SonarHit(gsc, 1f - (float) i/limit);
                     }
                 }
             }
@@ -213,7 +274,7 @@ namespace SoundScape.GameplaySceneComponents
         }
 
         /// <summary>
-        /// Draw the player at their current location.
+        ///     Draw the player at their current location.
         /// </summary>
         /// <param name="gameTime"></param>
         public override void Draw(GameTime gameTime)
@@ -221,69 +282,34 @@ namespace SoundScape.GameplaySceneComponents
             Color vColor = Colour;
             SpriteBatch.Begin();
             if (_aimVectors != null)
-            for (int i = 5; i < _aimVectors.Length; i++)
-            {
-                Vector2 aimVector = _aimVectors[i];
-                SpriteBatch.Draw(SonarTexture, aimVector - new Vector2(SonarTexture.Width / 2f, SonarTexture.Height / 2f), vColor);
-                vColor.R = (byte)(vColor.R / 1.1);
-                vColor.G = (byte)(vColor.G / 1.1);
-                vColor.B = (byte)(vColor.B / 1.1);
-            }
+                for (int i = 5; i < _aimVectors.Length; i++)
+                {
+                    Vector2 aimVector = _aimVectors[i];
+                    SpriteBatch.Draw(SonarTexture,
+                        aimVector - new Vector2(SonarTexture.Width/2f, SonarTexture.Height/2f), vColor);
+                    vColor.R = (byte) (vColor.R/1.1);
+                    vColor.G = (byte) (vColor.G/1.1);
+                    vColor.B = (byte) (vColor.B/1.1);
+                }
             SpriteBatch.End();
             base.Draw(gameTime);
         }
 
         /// <summary>
-        /// The left motor's rumble percentage.
+        ///     Cause the controller to rumble for a specified time.
         /// </summary>
-        public float RumbleLeft
-        {
-            get { return _rumbleLeft; }
-            set
-            {
-                if (Math.Abs(_rumbleLeft - value) > TOLLERENCE)
-                {
-                    _rumbleLeft = value;
-                    GamePad.SetVibration(Controller.PlayerIndex, _rumbleLeft, _rumbleRight);
-                }
-            }
-        }
-
-        /// <summary>
-        /// The right motor's rumble percentage.
-        /// </summary>
-        public float RumbleRight
-        {
-            get { return _rumbleRight; }
-            set
-            {
-                if (Math.Abs(_rumbleRight - value) > TOLLERENCE)
-                {
-                    _rumbleRight = value;
-                    GamePad.SetVibration(Controller.PlayerIndex, _rumbleLeft, _rumbleRight);
-                }
-            }
-        }
-
-
-        /// <summary>
-        /// Control the audio channel that sound is played on for this player.
-        /// </summary>
-        public float Pan
-        {
-            get { return _pan; }
-            set { _pan = value; }
-        }
-
-        /// <summary>
-        /// Cause the controller to rumble for a specified time.
-        /// </summary>
-        /// <param name="miliseconds">The time in miliseconds to rumble
-        /// the player's remote for.</param>
-        /// <param name="leftMotor">The percentage of power to use for
-        /// the left motor.</param>
-        /// <param name="rightMotor">The percentage of power to use for 
-        /// the right motor.</param>
+        /// <param name="miliseconds">
+        ///     The time in miliseconds to rumble
+        ///     the player's remote for.
+        /// </param>
+        /// <param name="leftMotor">
+        ///     The percentage of power to use for
+        ///     the left motor.
+        /// </param>
+        /// <param name="rightMotor">
+        ///     The percentage of power to use for
+        ///     the right motor.
+        /// </param>
         public void RumbleFor(int miliseconds, float leftMotor = -1f, float rightMotor = -1f)
         {
             if (leftMotor > 0)
@@ -295,7 +321,7 @@ namespace SoundScape.GameplaySceneComponents
             if (rightMotor > 0)
             {
                 _rumbleRightTime = DateTime.Now.AddMilliseconds(miliseconds);
-                _rumbleRight = rightMotor;                
+                _rumbleRight = rightMotor;
             }
 
             if (Enabled)
@@ -312,25 +338,20 @@ namespace SoundScape.GameplaySceneComponents
             base.OnEnabledChanged(sender, args);
         }
 
-        public override int Score
-        {
-            get { return SCORE; }
-        }
-
-        public Texture2D SonarTexture { get; set; }
-
-        public VirtualController Controller
-        {
-            get { return _controller; }
-            set { _controller = value; }
-        }
-
         public override void Kill()
         {
             if (Controller.ButtonDown(Buttons.LeftShoulder))
-                RumbleFor(miliseconds: 75, leftMotor: 1);
+                RumbleFor(75, 1);
             else
                 base.Kill();
+        }
+
+        private enum WeaponState
+        {
+            Charged,
+            Charging,
+            Discharged,
+            Cooldown
         }
     }
 }
