942ea166bbac3fc3d43467e10073655f134ab6bd
John Steel <jsteel-cc@conestogac.on.ca>
2014-12-05 12:45:28 -0500

    Added scale option to status

--- SoundScape/SoundScape/ControllerStatus.cs
+++ SoundScape/SoundScape/ControllerStatus.cs
@@ -25,10 +25,8 @@ namespace SoundScape
 
         protected override void LoadContent()
         {
-            _connectedTexture = Game.Content.Load<Texture2D>("images/controller/connected");
-            _disconnectedTexture = Game.Content.Load<Texture2D>("images/controller/disconnected");
-            _textureWidth = (_connectedTexture.Width + _disconnectedTexture.Width) / 2;
-            _textureHeight = (_connectedTexture.Height + _disconnectedTexture.Height) / 2;
+            ControllerData.ConnectedTexture = Game.Content.Load<Texture2D>("images/controller/connected");
+            ControllerData.DisconnectedTexture = Game.Content.Load<Texture2D>("images/controller/disconnected");
             base.LoadContent();
         }
 
@@ -40,25 +38,24 @@ namespace SoundScape
         public override void Draw(GameTime gameTime)
         {
             Vector2 marginY = Vector2.UnitY * MarginY;
-            Vector2 marginX = Vector2.UnitX*MarginX;
-            Vector2 windowWidth = Vector2.UnitX * (Game.Window.ClientBounds.Width - _textureWidth) - marginX*2;
-            Vector2 windowHeight = Vector2.UnitY * (Game.Window.ClientBounds.Height - _textureHeight);
+            Vector2 marginX = Vector2.UnitX * MarginX;
+            Vector2 windowWidth = Vector2.UnitX * (Game.Window.ClientBounds.Width - ControllerData.ConnectedTexture.Width * ControllerData.Scale) - marginX * 2;
+            Vector2 windowHeight = Vector2.UnitY * (Game.Window.ClientBounds.Height - ControllerData.ConnectedTexture.Height * ControllerData.Scale);
             _spritebatch.Begin();
             foreach (var player in new []
             {
                 new ControllerData { 
                     Connected = Game.PlayerOne.Connected, 
-                    Colour = Color.Red,
+                    Colour = Color.Pink,
                     Position = marginX - marginY + windowHeight}, 
                 new ControllerData { 
                     Connected = Game.PlayerTwo.Connected, 
-                    Colour = Color.Blue,
+                    Colour = Color.LightBlue,
                     Position = marginX - marginY + windowWidth + windowHeight}
             })
             {
-                Texture2D texture = player.Connected ? _connectedTexture : _disconnectedTexture;
-
-                _spritebatch.Draw(texture, player.Position, player.Colour);
+                _spritebatch.Draw(player.Texture,
+                    new Rectangle((int)player.Position.X, (int)player.Position.Y, player.TextureWidth, player.TextureHeight), player.Colour);
             }
             _spritebatch.End();           
             base.Draw(gameTime);
@@ -66,9 +63,36 @@ namespace SoundScape
 
         private struct ControllerData
         {
+            public static Texture2D ConnectedTexture;
+            public static Texture2D DisconnectedTexture;
+            public const int Scale = 3;
             public bool Connected;
             public Color Colour;
             public Vector2 Position;
+
+            public Texture2D Texture
+            {
+                get
+                {
+                    return Connected ? ConnectedTexture : DisconnectedTexture;
+                }
+            }
+
+            public int TextureWidth
+            {
+                get
+                {
+                    return (Connected ? ConnectedTexture.Width : DisconnectedTexture.Width) * Scale;
+                }
+            }
+
+            public int TextureHeight
+            {
+                get
+                {
+                    return (Connected ? ConnectedTexture.Height : DisconnectedTexture.Height) * Scale;
+                }
+            }
         }
     }
 }
